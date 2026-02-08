c99b35b2ea1a6538c6ff05b5994696eabd137eee
Jeffrey Smith <jasafpro@gmail.com>
2026-02-08 13:29:33 -0500

    updating patch version

--- js/version.js
+++ js/version.js
@@ -3,6 +3,6 @@
  * Single source of truth for version numbering
  */
 
-export const VERSION = '0.7.0-beta';
+export const VERSION = '0.7.0-r0';
 export const APP_NAME = 'AI Editor';
 export const VERSION_DISPLAY = `${APP_NAME} v${VERSION}`;
