c42f962283a15cdcbbbccf3ba3b20071fb1fcbc1
Jeffrey Smith <jasafpro@gmail.com>
2026-02-08 13:07:53 -0500

    fixing http pathing

--- js/template-loader.js
+++ js/template-loader.js
@@ -19,7 +19,7 @@ export async function loadTemplate(name, useCache = true) {
     }
 
     try {
-        const response = await fetch(`html/${name}.html`);
+        const response = await fetch(`editor/html/${name}.html`);
         
         if (!response.ok) {
             throw new Error(`Failed to load template '${name}': ${response.status} ${response.statusText}`);
