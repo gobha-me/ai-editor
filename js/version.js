06cde248e4870b10ef552741a5a753f033589111
Jeffrey Smith <jasafpro@gmail.com>
2026-02-09 18:03:48 -0500

    fixed scaling issues

--- js/version.js
+++ js/version.js
@@ -3,6 +3,6 @@
  * Single source of truth for version numbering
  */
 
-export const VERSION = '0.7.4';
+export const VERSION = '0.7.5';
 export const APP_NAME = 'AI Editor';
 export const VERSION_DISPLAY = `${APP_NAME} v${VERSION}`;
