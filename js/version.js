4e1b56c2a4da8b6a81f705e9c7a4e67438acb327
xcaliber <jasafpro@gmail.com>
2026-02-09 13:40:40 +0000

    Update js/version.js

--- js/version.js
+++ js/version.js
@@ -3,6 +3,6 @@
  * Single source of truth for version numbering
  */
 
-export const VERSION = '0.7.3-4';
+export const VERSION = '0.7.3-5';
 export const APP_NAME = 'AI Editor';
 export const VERSION_DISPLAY = `${APP_NAME} v${VERSION}`;
