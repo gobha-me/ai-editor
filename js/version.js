718d83a7cf8ca64b823701926d8eadcc48f977ad
xcaliber <jasafpro@gmail.com>
2026-02-14 21:19:45 +0000

    Ci updates deployment (#138)

--- js/version.js
+++ js/version.js
@@ -3,6 +3,6 @@
  * Single source of truth for version numbering
  */
 
-export const VERSION = '0.9.33-3';
+export const VERSION = '0.9.33-4';
 export const APP_NAME = 'AI Editor';
 export const VERSION_DISPLAY = `${APP_NAME} v${VERSION}`;
